f6e2e7df88479d3abe7be126674e5c1d37bf9873
Jordan Layton <jordan.layton@hotmail.com>
2022-04-02 23:47:12 +0100

    added workout, bt, hompeage and tech tips

--- src/app/events/events.page.ts
+++ src/app/events/events.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-events',
   templateUrl: './events.page.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+  postcode: string = "";
+  entered: string = "";
+  enteredBool = false;
+  postcodeEntered()
+  {
+  	this.entered = this.postcode;
+    this.enteredBool = true;
+    // this.router.navigate(['/identity'],{
+    //   queryParams: JSON.Stringify(data)
+    // })
+  }
 }
--- src/app/workouts/workouts.page.ts
+++ src/app/workouts/workouts.page.ts
@@ -8,8 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class WorkoutsPage implements OnInit {
 
   constructor() { }
-
+  continueBool = false;
+  levelBool = false;
+  level = ''
+  conditions = ''
+  healthYesBool = false;
+  healthNoBool = false;
+  selectedBool = false;
   ngOnInit() {
   }
-
+  tapToContinue(){
+    this.continueBool = true;
+  }
+  saveBeginnerLevel(){
+    this.level = 'Beginner';
+    this.levelBool = true;
+  }
+  saveIntermediateLevel(){
+    this.level = 'Intermediate';
+    this.levelBool = true;
+  }
+  saveAdvancedLevel(){
+    this.level = 'Advanced';
+    this.levelBool = true;
+  }
+  onYes(){
+    this.healthYesBool = true;
+  }
+  onNo(){
+    this.healthNoBool = true;
+  }
+  selectedConditions(){
+    // redundant
+    // this.conditions = this.conditions;
+    console.log("print print", this.selectedBool)
+    this.selectedBool = true;
+    console.log("print print", this.selectedBool)
+  }
 }
